e43cdc8cebaf03641d83b8b9f90e4a32a386d2b0
Dan Falconer <github@crazedsanity.com>
2015-10-20 22:30:24 -0500

    Initial commit.

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+//echo "RUNNING (". __FILE__ .")!!!!\n";
+
+// set the timezone to avoid spurious errors from PHP
+date_default_timezone_set("America/Chicago");
+
+if(file_exists(__DIR__ .'/../vendor/autoload.php')) {
+	require_once(__DIR__ .'/../vendor/autoload.php');
+}
+else {
+	trigger_error("vendor autoloader not found, unit tests will probably fail -- try running 'composer update'");
+}
+
+define('UNITTEST__LOCKFILE', __DIR__ .'/files/rw');
+
